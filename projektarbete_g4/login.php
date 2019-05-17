0ed6b93e8c4d3efd10f31af249e0935fee054aa5
alicekadhammar <31402599+alicekadhammar@users.noreply.github.com>
2019-05-17 13:43:02 +0200

    Update login.php

--- projektarbete_g4/login.php
+++ projektarbete_g4/login.php
@@ -1,1 +1,21 @@
-x
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="assets/css/mall.css">
+        <title></title>
+
+    </head>
+    <body>
+
+    </body>
+    </html>
+
+    <?php
+           <form name="form_login" action="process.php" method="_GET" onsubmit="return check()">
+                E-postadress:<br></br><input type="text" name="Mail"></br>
+                Password:<br></br><input type="text" name="Password"></br>
+                <input type="submit" value="logga in">
+
+            </form>
+?>
