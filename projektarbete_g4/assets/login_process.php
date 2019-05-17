b1d753ea1a7e92c48c7f09460a657b6d7f048f19
alicekadhammar <31402599+alicekadhammar@users.noreply.github.com>
2019-05-17 13:54:34 +0200

    Update login_process.php

---
+++ projektarbete_g4/assets/login_process.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="assets/css/mall.css">
+        <title></title>
+
+    </head>
+    <body>
+    </body>
+    </html>
+
+    <?php
+    $server  = "dbtrain.im.uu.se";
+     $username = "dbtrain_851";
+     $password = "todopx";
+     $dbname = "dbtrain_851";
+
+     // Create connection
+     $conn = new mysqli($server, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+    ?>
