60151534409be60447db66e4cdb6321cd6a841db
LeviHielleBergstrom <50480276+LeviHielleBergstrom@users.noreply.github.com>
2019-05-28 13:37:52 +0200

    Update process_com_del.php

--- BSprojektarbete_g4/process_com_del.php
+++ BSprojektarbete_g4/process_com_del.php
@@ -1,32 +1,18 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="assets/css/mall.css">
-        <title>Tvättstugan</title>
- </head>
-    <body>
-    </body>
-    </html>
 
     <?php
-		session_start();
-        $Name = $_SESSION['Name'];
-        $Mail = $_SESSION['Mail'];
-        $Comment = $_GET["Kommentar"];
 
-<<<<<<< HEAD
-        deleteCommentFromDatabase($Name,$Mail,$Comment);
-=======
+        $Id = $_GET["Id"];
 
-        addCommentTodatabase($Name,$Mail,$Comment);
->>>>>>> 79ffe1765f92029969b36af5122a3a0d41c00bf4
-        
-function deleteCommentFromDatabase($Name,$Mail,$Comment)
+        deleteCommentFromDatabase($Id);
+
+
+
+function deleteCommentFromDatabase($Id)
  {
      include "database.php";
 
-$sql = "DELETE FROM Comments (name,email,comment) VALUES ('$Name','$Mail','$Comment')";
+$sql = "DELETE FROM Comments WHERE id='$Id'";
 
 if ($conn->query($sql) === TRUE) {
     echo "Din kommentar har raderats från forumet";
