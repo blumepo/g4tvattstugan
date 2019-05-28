47e858551f1cb1f6a01de816b3aeacbb0bf38218
LeviHielleBergstrom <50480276+LeviHielleBergstrom@users.noreply.github.com>
2019-05-28 13:07:30 +0200

    com_del

--- BSprojektarbete_g4/process_com.php
+++ BSprojektarbete_g4/process_com.php
@@ -15,16 +15,16 @@
         $Mail = $_SESSION['Mail'];
         $Comment = $_GET["Kommentar"];
 
-        addCommentTodatabase($Name,$Mail,$Comment);
+        deleteCommentFromDatabase($Name,$Mail,$Comment);
         
-function addCommentToDatabase($Name,$Mail,$Comment)
+function deleteCommentFromDatabase($Name,$Mail,$Comment)
  {
      include "database.php";
 
-$sql = "INSERT INTO Comments (name,email,comment) VALUES ('$Name','$Mail','$Comment')";
+$sql = "DELETE FROM Comments (name,email,comment) VALUES ('$Name','$Mail','$Comment')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Din kommentar har postats i forumet";
+    echo "Din kommentar har raderats från forumet";
 } else {
     echo "Något fick fel: " . $sql . "<br>" . $conn->error;
 }
